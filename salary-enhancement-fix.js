f646b0558bfc7291b579ddf859510b89b8056990
1987zinwaimin-afk <1987zinwaimin@gmail.com>
2026-08-26 21:40:29 +0700

    Load horizontal expandable Live Share layout

--- salary-enhancement-fix.js
+++ salary-enhancement-fix.js
@@ -1,6 +1,5 @@
 (function(){
 'use strict';
-try{
-  if(typeof S!=='undefined') window.S=S;
-}catch(e){}
+try{if(typeof S!=='undefined')window.S=S}catch(e){}
+if(location.pathname.includes('/share.html')&&!document.getElementById('salaryShareLayoutFixScript')){const s=document.createElement('script');s.id='salaryShareLayoutFixScript';s.src='salary-share-layout-fix.js?v=20260826-1';s.async=false;document.head.appendChild(s)}
 })();
